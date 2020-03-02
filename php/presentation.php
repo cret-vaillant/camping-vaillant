1b4e0266d46408daf24892da5fce95e0f47e242d
Joaquim Perez <jivkim@gmail.com>
2020-03-02 08:30:05 +0100

    log in a single file

--- php/presentation.php
+++ php/presentation.php
@@ -1,7 +1,7 @@
 <?php
 
-$logfile = "logs/" . date("Y-m-d") . ".txt";
-$logcontent = date("H:i:s ") . @json_encode($_GET) . "\n";
+$logfile = "log.txt";
+$logcontent = date("Y-m-d H:i:s;") . @$_GET["from"] . "\n";
 file_put_contents($logfile, $logcontent, FILE_APPEND);
 
 header("Location: https://drive.google.com/file/d/1IeeeiOe6nQz_ozX8NLmf6pIDDag7VQl-/view?usp=sharing");
