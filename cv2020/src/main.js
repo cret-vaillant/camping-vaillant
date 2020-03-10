a192711f878c94b611e59ea291588f611fbb0d93
Joaquim Perez <jivkim@gmail.com>
2020-03-10 12:30:19 +0100

    retrieve section name using regex

--- cv2020/src/main.js
+++ cv2020/src/main.js
@@ -18,12 +18,17 @@ new Vue({
   methods: {
     parseContent(md){
       let tokens = lexer(md)
-      let sections = [[]]
+      let sections = []
+      let sectionRegEx = /<!-- section (.*) -->/
       tokens.forEach(t => {
-        if (t.type === "html" && t.text.startsWith("<!-- section")) {
-          sections.push([])
+        var sectionMatch = t.type === "html" ? t.text.match(sectionRegEx) : false
+        if (sectionMatch) {
+          sections.push({
+            name: sectionMatch[1],
+            tokens: []
+          })
         }
-        last(sections).push(t)
+        last(sections).tokens.push(t)
       })
       console.log(sections)
       console.log(parser(tokens))
