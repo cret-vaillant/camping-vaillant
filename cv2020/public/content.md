73919e017da3ffaad5c13f3f09da6acb6c6f19a8
Joaquim Perez <jivkim@gmail.com>
2020-03-10 12:07:56 +0100

    fetch and parse content.md by extracting section from html comments

---
+++ cv2020/public/content.md
@@ -0,0 +1,11 @@
+<!-- section 1 -->
+
+# Hello
+
+Salut
+
+<!-- section 2 -->
+
+# Bye
+
+Ok ciao
--- cv2020/src/main.js
+++ cv2020/src/main.js
@@ -1,10 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { get } from 'axios'
+import { lexer, parser } from 'marked'
+import { last } from 'lodash'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  created(){
+    get("content.md")
+      .then(r => r.data)
+      .then(this.parseContent)
+  },
+  methods: {
+    parseContent(md){
+      let tokens = lexer(md)
+      let sections = []
+      tokens.forEach(t => {
+        if (t.type === "html" && t.text.startsWith("<!-- section")) {
+          sections.push([])
+        }
+        last(sections).push(t)
+      })
+      console.log(sections)
+      console.log(parser(tokens))
+    }
+  }
 }).$mount('#app')
