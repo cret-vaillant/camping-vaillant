1191906e32c201980f24ae002f8fa8d0d1618a98
Joaquim Perez <jivkim@gmail.com>
2020-03-14 23:11:59 +0100

    remove temporary website files

--- php/presentation.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$logfile = "log.txt";
-$logcontent = date("Y-m-d H:i:s;") . @$_GET["from"] . "\n";
-file_put_contents($logfile, $logcontent, FILE_APPEND);
-
-header("Location: https://drive.google.com/file/d/1IeeeiOe6nQz_ozX8NLmf6pIDDag7VQl-/view?usp=sharing");
