1d8de8f5802ff641d57173476747f40186142c07
Joaquim Perez <jivkim@gmail.com>
2020-03-15 22:44:14 +0100

    main component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <cv-header/>
-    <router-view/>
+    <b-container tag="main" class="p-3">
+      <router-view/>
+    </b-container>
   </div>
 </template>
 
@@ -20,6 +22,13 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #222222;
+  main {
+    background-color: white;
+    margin-top: 8rem;
+    margin-bottom: 6rem;
+    position: relative;
+    min-height: 400px;
+  }
 }
 body {
   background-color: aquamarine !important;
