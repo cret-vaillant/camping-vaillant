fa60e9007715397f680d6a546869cb4429a90469
Joaquim Perez <jivkim@gmail.com>
2020-03-16 22:47:22 +0100

    review structure to have footer below background image

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <b-container>
-      <cv-header/>
-      <main class="p-3">
-        <router-view/>
-      </main>
-    </b-container>
+    <div>
+      <b-container>
+        <cv-header/>
+        <main class="p-3">
+          <router-view/>
+        </main>
+      </b-container>
+    </div>
     <footer>
-      Ici, le footer
+      <b-container fluid>
+        Ici, le footer
+      </b-container>
     </footer>
   </div>
 </template>
@@ -23,25 +27,33 @@ export default {
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i,900,900i&display=swap');
+
+body {
+  background-color: #84b4f5 !important;
+}
+
 #app {
   font-family: Lato, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #222222;
-  main {
-    background-color: white;
-    margin-top: 8rem;
-    margin-bottom: 6rem;
-    position: relative;
-    min-height: 400px;
+  > div {
+    background: linear-gradient(5deg,#bbd5fb 0%, #84b4f5 100%);
+    background-image: url("background.png");
+    background-repeat: no-repeat;
+    background-position: bottom;
+    background-size: contain;
+    padding-bottom: 180px;
+    main {
+      background-color: white;
+      margin-top: 8rem;
+      margin-bottom: 6rem;
+      position: relative;
+      min-height: 400px;
+    }
   }
-}
-body {
-  background-color: #84b4f5 !important;
-  background: linear-gradient(5deg,#bbd5fb 0%, #84b4f5 100%);
-  background-image: url("background.png");
-  background-repeat: no-repeat;
-  background-position: bottom;
-  background-size: contain;
+  > footer {
+    background: white;
+  }
 }
 </style>
