f639fa32430de489fad241d981466b7018aaeeb2
Joaquim Perez <jivkim@gmail.com>
2020-03-16 22:19:36 +0100

    sign min widths

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,7 +8,12 @@
         offset-lg="2" lg="5"
         offset-xl="3" xl="4"
       >
-        <sign rotate="-1" padding="3" stick-height="800">
+        <sign 
+          class="sign-1" 
+          rotate="-1" 
+          padding="3" 
+          stick-height="800"
+        >
           <p>Le</p>
           <h1>Camping Vaillant</h1>
         </sign>
@@ -22,8 +27,15 @@
         offset-lg="6" lg="4"
         offset-xl="6" xl="3"
       >
-        <sign rotate="2" stick-width="16" padding="2" stick-height="500" stick-top="0">
-          <h2>19 <small>&</small> 20 JUIN 2020</h2>
+        <sign
+          class="sign-2"
+          rotate="3"
+          stick-width="16"
+          padding="2"
+          stick-height="500"
+          stick-top="0"
+        >
+          <h2>19&nbsp;<small>&</small>&nbsp;20 JUIN 2020</h2>
         </sign>
       </b-col>
     </b-row>
@@ -34,7 +46,12 @@
         offset-md="3" md="6"
         offset-lg="4" lg="4"
       >
-        <sign padding="2" stick-width="12" stick-radius="0">
+        <sign
+          class="sign-3"
+          padding="3"
+          stick-width="12"
+          stick-radius="0"
+        >
           <h3>11<sup>ème</sup> fête du Crêt-Vaillant</h3>
           <div class="spacer m-2"></div>
           <h3>Le Locle</h3>
@@ -57,12 +74,13 @@ export default {
 <style lang="scss">
 header{
   .sign{
+    margin: auto;
     h1,h3{
       font-style: italic;
     }
     h1{
       font-weight: 900;
-      font-size: 3rem;
+      font-size: 3.5rem;
     }
     h2{
       font-weight: bold;
@@ -77,6 +95,12 @@ header{
     .spacer{
       background-color: #222222;
       height: 2px;
+    }
+    &.sign-1{
+      max-width: 300px;
+    }
+    &.sign-3{
+      max-width: 270px;
     }
   }
 }
