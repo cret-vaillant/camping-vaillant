bf56ff9af826bf303f27acd4b8de301881c2a846
Joaquim Perez <jivkim@gmail.com>
2020-03-18 22:50:00 +0100

    retrieve sponsors from google sheet

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import { get } from 'axios'
 import { lexer, parser } from 'marked/lib/marked'
 import { last } from 'lodash'
+import { zipObject } from 'lodash'
 import { LayoutPlugin } from 'bootstrap-vue'
 Vue.use(LayoutPlugin)
 import 'bootstrap/dist/css/bootstrap.css'
@@ -15,12 +16,15 @@ new Vue({
   router,
   render: h => h(App),
   data: {
-    sections: []
+    sections: [],
+    sponsors: [],
+    sponsorHeader: ""
   },
   created(){
     get("content.md")
       .then(r => r.data)
       .then(this.parseContent)
+      .then(this.fetchSponsors)
   },
   methods: {
     newSection(name){
@@ -45,6 +49,26 @@ new Vue({
         s.html = parser(s.tokens)
       })
       this.sections = sections
+    },
+    fetchSponsors(){
+      let url =
+        "https://sheets.googleapis.com/v4/spreadsheets/" +
+        process.env.VUE_APP_SPREADSHEET_ID + "/values/" +
+        process.env.VUE_APP_SPREADSHEET_RANGE +
+        "?key=" + process.env.VUE_APP_GOOGLE_API_KEY
+      
+        get(url).then(response => {
+          var data = response.data.values
+          console.log(data)
+          this.sponsorHeader = data.shift()
+          var header = data.shift()
+          data.forEach(row => {
+              var sponsor = zipObject(header, row)
+              if(sponsor.name){
+                  this.sponsors.push(sponsor)
+              }
+          })
+      })
     }
   }
 }).$mount('#app')
