de4068843aaf9f599d9502659d78959af74b4357
Joaquim Perez <jivkim@gmail.com>
2020-03-19 16:03:16 +0100

    rename Home -> Content

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Content from '../views/Content.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Content',
+    component: Content
   }
 ]
 
