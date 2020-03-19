7644413a47eb8110abd372a11edbcf1aa4f745b1
Joaquim Perez <jivkim@gmail.com>
2020-03-19 18:22:38 +0100

    use sign as nav link

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -6,9 +6,32 @@
         :key="section.name"
       >
         <router-link :to="section.name">
-          {{ section.name }}
+          <sign
+            pointable
+            stick-width="6"
+            stick-top="0"
+          >
+            {{ section.name }}
+          </sign>
         </router-link>
       </b-col>
     </b-row>
   </nav>
 </template>
+
+<script>
+import Sign from "@/components/Sign"
+export default {
+  components: { Sign }
+}
+</script>
+
+<style lang="scss" scoped>
+a{
+  color: inherit;
+  &:hover{
+    text-decoration: none;
+    color: inherit;
+  }
+}
+</style>
