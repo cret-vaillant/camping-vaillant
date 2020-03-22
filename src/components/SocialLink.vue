7001bcf25c6b3103ec4ed9a764aed15957cf10b2
Joaquim Perez <jivkim@gmail.com>
2020-03-22 22:00:46 +0100

    new component SocialLink

---
+++ src/components/SocialLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <a :href="'https://' + url" target="_blank"
+      :style="{
+        width: '32px',
+        height: '32px',
+        margin: 'auto',
+        display: 'block',
+        backgroundImage: 'url(social/' + imgSrc + ')',
+      }"
+  />
+</template>
+
+<script>
+export default {
+  props: ["url"],
+  computed: {
+    imgSrc() {
+      return this.url.split(".")[0] + ".png"
+    }
+  }
+}
+</script>
