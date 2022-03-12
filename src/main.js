953c44326c2de85a7dd06b375333016b05928f87
Joaquim Perez <jivkim@gmail.com>
2022-03-12 21:52:10 +0100

    remove content.md related stuff

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { get } from 'axios'
-import { lexer, parser } from 'marked/lib/marked'
-import { last, zipObject } from 'lodash'
+import { zipObject } from 'lodash'
 import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -21,35 +20,9 @@ new Vue({
     sponsorHeader: ""
   },
   created(){
-    get("content.md")
-      .then(r => r.data)
-      .then(this.parseContent)
-      .then(this.fetchSponsors)
+    this.fetchSponsors()
   },
   methods: {
-    newSection(name){
-      var tokens = []
-      tokens.links = {}
-      return { name, tokens }
-    },
-    parseContent(md){
-      var tokens = lexer(md)
-      let sections = []
-      let sectionRegEx = /<!-- section (.*) -->/
-      tokens.forEach(t => {
-        var sectionMatch = t.type === "html" ? t.text.match(sectionRegEx) : false
-        if (sectionMatch) {
-          sections.push(this.newSection(sectionMatch[1]))
-        } else if (sections.length === 0) {
-          sections.push(this.newSection("first section"))
-        }
-        last(sections).tokens.push(t)
-      })
-      sections.forEach(s => {
-        s.html = parser(s.tokens)
-      })
-      this.sections = sections
-    },
     fetchSponsors(){
       let url =
         "https://sheets.googleapis.com/v4/spreadsheets/" +
@@ -62,10 +35,10 @@ new Vue({
           this.sponsorHeader = data.shift()[0]
           var header = data.shift()
           data.forEach(row => {
-              var sponsor = zipObject(header, row)
-              if(sponsor.name){
-                  this.sponsors.push(sponsor)
-              }
+            var sponsor = zipObject(header, row)
+            if(sponsor.name){
+              this.sponsors.push(sponsor)
+            }
           })
       })
     }
