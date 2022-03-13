84cdf51d1d6240a45c7afc0075c89a146ef22265
Joaquim Perez <jivkim@gmail.com>
2022-03-13 22:45:33 +0100

    post title layout

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -32,11 +32,17 @@ export default {
 
 <style lang="scss">
 .post {
+  h3 {
+    font-size: 1.3rem;
+    font-weight: 900;
+    font-style: italic;
+  }
+
   .excerpt {
     height: 200px;
     padding: 20px;
     box-shadow: 0 4px 8px 0 rgba(0,0,0,0.1);
-    margin: 10px 0 20px 0;
+    margin: 6px 0 20px 0;
     &.sticky {
       background-image: linear-gradient(45deg, #f6a34f, #f43136, #c31b1d);
       color: white;
