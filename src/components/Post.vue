2ce92d805f2ee957ffe819062992f4503e18215b
Joaquim Perez <jivkim@gmail.com>
2022-03-13 22:31:32 +0100

    post component can show a post or a product

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="post">
-    <h3 v-html="title.rendered"></h3>
+    <h3 v-html="title"></h3>
     <div
       class="excerpt"
       :class="sticky ? 'sticky' : ''"
-      v-html="excerpt.rendered"
+      v-html="excerpt"
     ></div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['title', 'excerpt', 'sticky']
+  props: ['item'],
+
+  computed: {
+    isPost() {
+      return this.item._type === 'post'
+    },
+    title() {
+      return this.isPost ? this.item.title.rendered : this.item.name
+    },
+    excerpt() {
+      return this.isPost ? this.item.excerpt.rendered : this.item.description
+    },
+    sticky() {
+      return this.isPost ? this.item.sticky : this.item.featured
+    }
+  }
 }
 </script>
 
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,26 +3,24 @@
     <div v-if="$root.items.length === 0">
       Chargement...
     </div>
-    <article
+    <post
       v-for="item in $root.items"
       :key="item.id"
-    >
-      <component :is="item._type" v-bind="item"></component>
-    </article>
+      :item="item"
+    />
   </div>
 </template>
 
 <script>
 import Post from '@/components/Post'
-import Product from '@/components/Product'
 
 export default {
-  components: { Post, Product }
+  components: { Post }
 }
 </script>
 
 <style scoped>
-article {
+.post {
   margin-bottom: 40px;
 }
 </style>
