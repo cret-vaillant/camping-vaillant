3f2a7868a2736d3085fe6286c2ac8e5f2a8eabdd
Joaquim Perez <jivkim@gmail.com>
2022-03-14 00:25:11 +0100

    add a add-to-calendar-button

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
+  <div class="index">
+    <div class="calendar">
+      <small><i>Ajoutez la fête à votre calendrier:</i></small>
+      <div class="atcb" style="display:none;">
+        {
+          "name":"Le Camping Vaillant",
+          "description":"La 11ème fête du Crêt-Vaillant\nConcerts, jeux et stands\nToutes les infos sur [url]https://cret-vaillant.ch[/url]\nYoupie!",
+          "startDate":"2022-06-17",
+          "endDate":"2022-06-19",
+          "startTime":"16:30",
+          "endTime":"02:00",
+          "location":"Fête du Crêt-Vaillant, Crêt-Vaillant 16-30, 2400 Le Locle, Suisse",
+          "label":"17 & 18 juin 2022",
+          "options":[
+            "Apple",
+            "Google",
+            "Outlook.com"
+          ],
+          "timeZone":"Europe/Zurich",
+          "trigger":"click",
+          "iCalFileName":"camping-vaillant"
+        }
+      </div>
+    </div>
     <div v-if="$root.items.length === 0">
       Chargement...
     </div>
@@ -12,15 +35,32 @@
 </template>
 
 <script>
+import { atcb_init } from 'add-to-calendar-button'
+import 'add-to-calendar-button/assets/css/atcb.min.css'
 import Post from '@/components/Post'
 
 export default {
-  components: { Post }
+  components: { Post },
+
+  mounted: atcb_init
 }
 </script>
 
-<style scoped>
-.post {
-  margin-bottom: 40px;
+<style lang="scss">
+.index {
+  .calendar {
+    text-align: center;
+    .atcb_initialized {
+      margin-top: -10px;
+      margin-bottom: 40px;
+      .atcb_button {
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.1);
+        border: 2px solid #ffffff;
+      }
+    }
+  }
+  .post {
+    margin-bottom: 40px;
+  }
 }
 </style>
